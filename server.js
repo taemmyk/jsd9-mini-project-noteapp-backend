ae33e1a9df0808fef6a1117700ba40554e071eff
tk <t.khamiam@gmail.com>
2025-05-04 19:50:06 +0700

    feat(server): connected to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import chalk from "chalk";
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -8,6 +9,16 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+
+(async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(chalk.green("Connected to Mongo database"));
+  } catch (err) {
+    console.error(chalk.red(`MongoDB connection error ${err}`));
+    process.exit(1);
+  }
+})();
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${chalk.blue(`http://localhost:${PORT}`)}`);
