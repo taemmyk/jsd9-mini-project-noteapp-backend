f10308a2104d28513784949f1677d1189311a207
tk <t.khamiam@gmail.com>
2025-05-04 21:59:23 +0700

    feat(routes): updated delete a note route

--- api/v1/controllers/notesController.js
+++ api/v1/controllers/notesController.js
@@ -4,7 +4,7 @@ import { Note } from "../../../models/Note.js";
 export const getAllNotes = async (req, res, next) => {
   try {
     const notes = await Note.find().sort({ createdAt: -1 });
-    res.status(200).json(notes);
+    res.status(200).json({ error: false, notes });
   } catch (err) {
     next(err);
   }
@@ -26,7 +26,7 @@ export const createNewNote = async (req, res, next) => {
       content,
       userId,
     });
-    res.status(201).json(note);
+    res.status(201).json({ error: false, note });
   } catch (err) {
     next(err);
   }
@@ -59,9 +59,36 @@ export const updateNote = async (req, res, next) => {
 
     if (updateNoteResult.modifiedCount > 0) {
       const updatedNote = await Note.findById(id);
-      res.status(200).json(updatedNote);
+      res.status(200).json({ error: false, updatedNote });
     } else {
-      res.status(200).json({ message: "No changes made to the note" });
+      res
+        .status(200)
+        .json({ error: false, message: "No changes made to the note" });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+//? delete a note by a specific id
+export const deleteNote = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const targetNote = await Note.findById(id);
+    if (!targetNote) {
+      return res.status(404).json({
+        error: true,
+        message: "Note not found",
+      });
+    }
+
+    const deleteResult = await Note.deleteOne({ _id: id });
+    if (deleteResult.deletedCount > 0) {
+      res
+        .status(204)
+        .json({ error: false, message: "Delete note successfully" });
+    } else {
+      res.status(404).json({ error: true, message: "Note not found" });
     }
   } catch (err) {
     next(err);
--- api/v1/routes/notes.js
+++ api/v1/routes/notes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
-import { getAllNotes, createNewNote, updateNote } from "../controllers/notesController.js";
+import { getAllNotes, createNewNote, updateNote, deleteNote } from "../controllers/notesController.js";
 
 const router = express.Router();
 
 router.get("/", getAllNotes);
 router.post("/", createNewNote);
 router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
 
 export default router;
