d87a410bdfcd461f225d3f1dfc51b6bb0916701d
tk <t.khamiam@gmail.com>
2025-05-04 21:44:49 +0700

    feat(routes): updated update a note route

--- api/v1/controllers/notesController.js
+++ api/v1/controllers/notesController.js
@@ -31,3 +31,39 @@ export const createNewNote = async (req, res, next) => {
     next(err);
   }
 };
+
+//? edit a note by a specific id
+export const updateNote = async (req, res, next) => {
+  const { id } = req.params;
+  const { title, content, userId } = req.body;
+  try {
+    if (!title || !content || !userId) {
+      return res.status(400).json({
+        error: true,
+        message: "Title, content and user ID are required",
+      });
+    }
+
+    const targetNote = await Note.findById(id);
+    if (!targetNote) {
+      return res.status(404).json({
+        error: true,
+        message: "Note not found",
+      });
+    }
+
+    const updateNoteResult = await Note.updateOne(
+      { _id: id },
+      { title, content, userId, updatedAt: new Date().getTime() }
+    );
+
+    if (updateNoteResult.modifiedCount > 0) {
+      const updatedNote = await Note.findById(id);
+      res.status(200).json(updatedNote);
+    } else {
+      res.status(200).json({ message: "No changes made to the note" });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
--- api/v1/routes/notes.js
+++ api/v1/routes/notes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
-import { getAllNotes, createNewNote } from "../controllers/notesController.js";
+import { getAllNotes, createNewNote, updateNote } from "../controllers/notesController.js";
 
 const router = express.Router();
 
 router.get("/", getAllNotes);
 router.post("/", createNewNote);
+router.put("/:id", updateNote);
 
 export default router;
