b2a1cbab88b7d3abbdd7e6eebdeda051f833363c
tk <t.khamiam@gmail.com>
2025-05-05 09:42:21 +0700

    feat(route): planned user routes

---
+++ api/v1/controllers/userController.js
@@ -0,0 +1,10 @@
+import { User } from "../../../models/User.js";
+
+//TODO: register a new user
+export const createNewUser = async (req, res, next) => {};
+
+//TODO: sign in
+export const logUserIn = async (req, res, next) => {};
+
+//TODO: sign out
+export const logUserOut = async (req, res, next) => {};
--- api/v1/routes.js
+++ api/v1/routes.js
@@ -2,12 +2,16 @@ import express from "express";
 import NoteRoutes from "./routes/notes.js";
 import { getNoteReport } from "./controllers/reportsController.js";
 import { searchNote } from "./controllers/notesController.js";
+import { createNewUser, logUserIn, logUserOut } from "./controllers/userController.js";
 
 const router = express.Router();
 
 export default () => {
   router.use("/notes", NoteRoutes);
-  router.get("/report/notes", getNoteReport);
-  router.post("/search/notes", searchNote);
+  router.post("/reports", getNoteReport);
+  router.post("/search", searchNote);
+  router.post("/register", createNewUser);
+  router.post("/login", logUserIn);
+  router.post("/logout", logUserOut);
   return router;
 };
